0dd7f56608e8fe8e64d6f580d72f5abcb7a29489
Saif1005 <saifakkari.sa@gmail.com>
2024-11-01 00:38:06 +0100

    Ajout de mon projet de prédiction de crédit

---
+++ app.py
@@ -0,0 +1,19 @@
+import  numpy as np
+from flask import Flask, render_template, request, redirect, url_for
+import pickle
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0] * 100, 2)  # Convert to percentage
+    return render_template('index.html',prediction_text='Votre probabilité d\'obtenir un crédit est de {}%'.format(output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
